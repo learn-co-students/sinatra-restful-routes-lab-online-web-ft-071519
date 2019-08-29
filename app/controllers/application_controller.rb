f647bde71c75553aaafd7fb94282af6db912a1e0
Ahmed Abououf <aabuauf@yahoo.com>
2019-08-29 18:32:16 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,15 @@ class ApplicationController < Sinatra::Base
     @recipe = Recipe.find_by_id(params[:id])
     erb :show
   end
+  
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+  
+  # delete '/recipes/:id/delete' do #delete action
+  #   @recipe = Recipe.find_by_id(params[:id])
+  #   @article.delete
+  #   redirect to '/recipes'
+  # end
 end
