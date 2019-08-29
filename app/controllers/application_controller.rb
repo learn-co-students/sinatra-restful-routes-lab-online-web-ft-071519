1bf0fdfe455eef818c44b50b269b0ecc6dae7704
Ahmed Abououf <aabuauf@yahoo.com>
2019-08-29 18:54:14 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,21 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  get '/recipes/new' do
+    
+    erb :new
+  end
+  
+
   get '/recipes' do
     @allRecipes = Recipe.all
   erb :index
+  end
+  
+  post '/recipes' do
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
   end
   
   get '/recipes/:id' do
@@ -17,12 +29,22 @@ class ApplicationController < Sinatra::Base
   
   get '/recipes/:id/edit' do
     @recipe = Recipe.find_by_id(params[:id])
-    erb :show
+    erb :edit
   end
   
-  # delete '/recipes/:id/delete' do #delete action
-  #   @recipe = Recipe.find_by_id(params[:id])
-  #   @article.delete
-  #   redirect to '/recipes'
-  # end
+  patch '/recipes/:id' do #edit action
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+  
+
+  delete '/recipes/:id' do #delete action
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect to '/recipes'
+  end
 end
